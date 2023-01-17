1cce96eee7a6bb3e4ef375d91111ecb6df6aea80
Diana Opiyo <dopiyo@gmail.com>
2023-01-17 10:24:04 +0300

    added some more files and notes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-// Import express framework
+// Import express package
 const express = require('express');
+
 
 // Initialize express
 const app = express();
 
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+
+// Set a static folder
+app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>{
+   res.render('index');
+})
+
+
 // Define the port number
 const PORT = 5000;
 
-
-app.listen(PORT, function(){
+app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
-})
+});
