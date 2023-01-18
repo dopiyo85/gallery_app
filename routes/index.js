ef09c2fba0717eaf663d40c6c25d5b9d4733f727
Diana Opiyo <dopiyo@gmail.com>
2023-01-18 12:39:44 +0300

    connected db to my app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../upload');
+const mongoose = require("mongoose");
+
+let DB_NAME = process.env.DB_NAME;
+let DB_USER = process.env.DB_USER;
+let DB_PASS = process.env.DB_PASS;
+let DB_URL = process.env.DB_URL
+let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@galleryapp.h2prkcb.mongodb.net/" +DB_NAME;
+console.log(db_url);
+let option = {
+    maxPoolSize:10,
+    family:4
+};
+
+mongoose.connect(db_url,option);
+let db = mongoose.connection
+db.once("open",() => {
+    console.log("Successful connection to the database");
+});
+
+db.on("error", ()=> {
+    console.log("Connection to DB failed");
+});
 
 router.get('/', (req,res)=>{
    res.render('index');
