0b86f58669e3723561965ecb2d8ce6ce6e1a1149
Diana Opiyo <dopiyo@gmail.com>
2023-01-18 19:24:12 +0300

    connected to db

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../upload');
 const mongoose = require("mongoose");
-
-let DB_NAME = process.env.DB_NAME;
-let DB_USER = process.env.DB_USER;
-let DB_PASS = process.env.DB_PASS;
-let DB_URL = process.env.DB_URL
-let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@galleryapp.h2prkcb.mongodb.net/" +DB_NAME;
-console.log(db_url);
-let option = {
-    maxPoolSize:10,
-    family:4
-};
-
-mongoose.connect(db_url,option);
-let db = mongoose.connection
-db.once("open",() => {
-    console.log("Successful connection to the database");
-});
-
-db.on("error", ()=> {
-    console.log("Connection to DB failed");
-});
+const Image = require("./image");
 
 router.get('/', (req,res)=>{
    res.render('index');
@@ -35,6 +15,12 @@ router.post('/upload', (req,res)=>{
            console.log(err)
            res.render('index', {msg: err})
        }else{
+       let newImage = new Image({
+            name:req.file.filename,
+            path:'/public/images/'+ req.file.filename,
+            date:Date.now()
+        }) 
+        newImage.save();
            console.log(req.file);
            res.render('index', {file: 'images/' + req.file.filename})
        }
