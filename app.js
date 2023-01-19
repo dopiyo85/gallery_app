e0e847e553f9e6f61dcdb7436b56370aba132586
Diana Opiyo <dopiyo@gmail.com>
2023-01-19 19:33:01 +0300

    working app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 
 let indexRouter = require('./routes/index');
+let search_router = require('./routes/search');
 
 // Initialize express
 const app = express();
@@ -13,8 +14,14 @@ app.set('view engine', 'ejs');
 // Set a static folder
 app.use(express.static('public'));
 
+// formData => req.body
+app.use(express.json()); // application/json
+app.use(express.urlencoded({extended: true}));
+
+
 // Define the index router
 app.use('/', indexRouter);
+app.use("/", search_router);
 
 //connecting to the db
 const mongoose = require("mongoose");
